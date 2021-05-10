25d190918eb3c303fb65993efa2035aaf9368794
Amarendra Sharma <s.amarendra1995@gmail.com>
2021-05-10 21:06:07 +0530

    dist

---
+++ dist/precache-manifest.b5d7bab97e538c5f68369c8b39a5af17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dabc12992ab1ac16080b",
+    "url": "/css/app.638b448d.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "bed2e1df185f6e0dc4e5754676083105",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dabc12992ab1ac16080b",
+    "url": "/js/app.eca47d70.js"
+  },
+  {
+    "revision": "16b96474c75b627eb4f1",
+    "url": "/js/chunk-vendors.fa5d007e.js"
+  },
+  {
+    "revision": "9a60343d9830e135b0fd4362a7c924a9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
